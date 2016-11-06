fbf4566ebda83984bc767f16d9d2911d57e89058
Alexander Ten <orionapplepie@gmail.com>
2016-11-06 21:41:43 +1000

    123

--- common/models.py
+++ common/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=32, blank=False, null=False)
+    email = models.EmailField(null=False, blank=False)
+    subject = models.CharField(max_length=32)
+    message = models.CharField(max_length=255, null=False, blank=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    answer = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+'''test'''
